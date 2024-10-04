5f00aacf0a1eaa24a4b5413f3bbc903b026f0fa3
AlanAdamik <139379784+AlanAdamik@users.noreply.github.com>
2024-10-04 15:20:23 +0200

    Update app.controller.ts

--- backend-nestjs/src/app.controller.ts
+++ backend-nestjs/src/app.controller.ts
@@ -6,11 +6,13 @@ import { appointments } from './appointments';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  // THIS IS AN EXAMPLE OF A GET ENDPOINT
   @Get()
   getHello(): string {
     return this.appService.getHello();
   }
 
+  // THIS IS AN EXAMPLE OF HOW TO HANDLE REQUESTS WITH PARAMS (HERE name)
   @Get('/hello/:name')
   getHelloName(@Param('name') name: string): string {
     return this.appService.getHello(name);
