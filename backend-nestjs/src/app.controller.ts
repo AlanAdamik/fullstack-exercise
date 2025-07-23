24cff18b8fdbf20a65c89d5a5ce47656defdf5b9
Alan Adamik <alan@modaresa.com>
2025-07-23 12:43:41 +0200

    add query parameter example in backend

--- backend-nestjs/src/app.controller.ts
+++ backend-nestjs/src/app.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, Query } from '@nestjs/common';
 import { AppService } from './app.service';
 import { appointments } from './appointments';
 
@@ -6,10 +6,10 @@ import { appointments } from './appointments';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  // THIS IS AN EXAMPLE OF A GET ENDPOINT
+  // THIS IS AN EXAMPLE OF A GET ENDPOINT WITH AN OPTIONAL QUERY PARAMETER
   @Get()
-  getHello(): string {
-    return this.appService.getHello();
+  getHello(@Query('name') name: string): string {
+    return this.appService.getHello(name);
   }
 
   // THIS IS AN EXAMPLE OF HOW TO HANDLE REQUESTS WITH PARAMS (HERE name)
